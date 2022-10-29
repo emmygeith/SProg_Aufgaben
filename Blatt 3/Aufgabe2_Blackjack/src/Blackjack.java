7d2544842f58ed35c6114c6cc84fcc65439d9a5c
emmygeith <emmylou.geith@gmx.de>
2022-10-29 18:41:11 +0200

    Sonderfälle bedacht

--- Blatt 3/Aufgabe2_Blackjack/src/Blackjack.java
+++ Blatt 3/Aufgabe2_Blackjack/src/Blackjack.java
@@ -16,12 +16,14 @@ public class Blackjack {
 				System.out.println("Leider sind beide Zahlen groesser 21, es gewinnt die Bank!");
 			} else if (a == b) {
 				System.out.println("Die Zahlen sind gleich, bitte spielen Sie noch einmal");
-			} else if (a < 21 && b < 21) {
+			} else if (a <= 21 && b <= 21) {
 				if (a > b) {
 					System.out.println(a + " ist dichter an der 21");
 				} else {
 					System.out.println(b + " ist dichter an der 21");
 				}
+			} else if ((a <= 21 && b > 21) || (a > 21 && b <= 21)) {
+				System.out.println("falsche Eingabe");
 			}
 			System.out.println("Moechtest du noch eine Runde spielen? Wenn ja druecke '1'");
 			x = scanner.nextInt();
