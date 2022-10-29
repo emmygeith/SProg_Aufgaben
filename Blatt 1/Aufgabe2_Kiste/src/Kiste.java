e0e83b70df68132134c797b836d0f875f6c302b6
emmygeith <emmylou.geith@gmx.de>
2022-10-29 18:26:56 +0200

    Kiste Java Umlaute korrigiert

--- Blatt 1/Aufgabe2_Kiste/src/Kiste.java
+++ Blatt 1/Aufgabe2_Kiste/src/Kiste.java
@@ -3,26 +3,26 @@ import java.util.Scanner;
 public class Kiste {
 
 	public static void main(String[] args) {
-	float l, b, h, volumen, oberfläche, kantenlänge;
+		float l, b, h, volumen, oberflaeche, kantenlaenge;
 
-	Scanner scanner = new Scanner(System.in);
-	
-	System.out.println("Länge der Kiste: ");
-	l = scanner.nextFloat();
-	
-	System.out.println("Breite der Kiste: ");
-	b = scanner.nextFloat();
-	
-	System.out.println("Höhe der Kiste: ");
-	h = scanner.nextFloat();
-	
-	volumen = l * b * h ;
-	oberfläche = 2*(l*h+l*b+b*h);
-	kantenlänge = 4*(l+b+h);
-	
-	System.out.println("Volumen: " + volumen);
-	System.out.println("Oberfläche: " + oberfläche);
-	System.out.println("Kantenlänge: " + kantenlänge);
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Laenge der Kiste: ");
+		l = scanner.nextFloat();
+
+		System.out.println("Breite der Kiste: ");
+		b = scanner.nextFloat();
+
+		System.out.println("Hoehe der Kiste: ");
+		h = scanner.nextFloat();
+
+		volumen = l * b * h;
+		oberflaeche = 2 * (l * h + l * b + b * h);
+		kantenlaenge = 4 * (l + b + h);
+
+		System.out.println("Volumen: " + volumen);
+		System.out.println("Oberflaeche: " + oberflaeche);
+		System.out.println("Kantenlï¿½nge: " + kantenlaenge);
 	}
 
 }
